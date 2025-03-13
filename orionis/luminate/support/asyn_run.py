35315316d3080839aa721ee00b2cfb3af6da3274
Raul Mauricio Uñate Castro <runate@ccl.com.co>
2025-03-13 18:26:40 -0500

    📦 Release version 0.185.0

--- orionis/luminate/support/asyn_run.py
+++ orionis/luminate/support/asyn_run.py
@@ -1,17 +1,22 @@
 import asyncio
+from typing import Any, Callable
 
 class AsyncExecutor:
 
     @staticmethod
-    def run(callback: asyncio.coroutine) -> None:
+    def run(callback: Callable[..., Any]) -> None:
         """
         Runs a coroutine synchronously.
 
         Parameters
         ----------
-        callback : asyncio.coroutine
+        callback : Callable[..., Any]
             The coroutine to run.
         """
+
+        if not callable(callback):
+            raise TypeError("The given callback is not callable.")
+
         try:
             loop = asyncio.get_running_loop()
             loop.run_until_complete(callback)
