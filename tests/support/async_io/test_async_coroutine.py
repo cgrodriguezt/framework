2bfe12c8a6f755c1308ff9a3770d3e9cc7b7ba20
Raul Mauricio Uñate Castro <runate@ccl.com.co>
2025-05-01 22:19:39 -0500

    📦 Release version 0.220.0

--- tests/support/async_io/test_async_coroutine.py
+++ tests/support/async_io/test_async_coroutine.py
@@ -1,6 +1,5 @@
 from orionis.luminate.support.async_io.async_coroutine import AsyncCoroutine
 from orionis.luminate.test.test_case import TestCase
-from tests.support.adapters.fakes.fake_dict import fake_dict
 
 class TestsAsyncCoroutine(TestCase):
 
@@ -16,20 +15,24 @@ class TestsAsyncCoroutine(TestCase):
         result = await AsyncCoroutine.execute(sample_coroutine())
         self.assertEqual(result, "Hello, World!")
 
-    async def testExecuteWithoutActiveEventLoop(self):
+    def testExecuteWithoutActiveEventLoop(self):
         """
         Test the execution of a coroutine without an active event loop.
         This test simulates a scenario where the coroutine is executed in a synchronous context without an active event loop.
         """
         async def sample_coroutine():
             return "Hello, World!"
-        result = await AsyncCoroutine.execute(sample_coroutine())
+
+        result = AsyncCoroutine.execute(sample_coroutine())
         self.assertEqual(result, "Hello, World!")
 
-    async def testExecuteWithNonCoroutine(self):
+    def testExecuteWithNonCoroutine(self):
         """
         Test the execution of a non-coroutine object.
         This test checks that a TypeError is raised when a non-coroutine object is passed to the execute method.
         """
-        with self.assertRaises(TypeError) as context:
-            AsyncCoroutine.execute("not a coroutine")
+        def sample_no_coroutine():
+            return "Hello, World!"
+
+        with self.assertRaises(TypeError):
+            AsyncCoroutine.execute(sample_no_coroutine())
