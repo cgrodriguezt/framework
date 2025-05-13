08be727875b5374b59273b4639a7ca5e63fba3a6
Raul Mauricio Uñate Castro <runate@ccl.com.co>
2025-05-13 12:57:30 -0500

    Singleton MetaClass + DotEnv Service

--- orionis/luminate/patterns/__init__.py
+++ orionis/luminate/patterns/__init__.py
@@ -3,3 +3,9 @@ from orionis.luminate.patterns.singleton.meta_class import Singleton
 __all__ = [
     "Singleton",
 ]
+
+__author__ = "Raúl Mauricio Uñate Castro"
+__description__ = (
+    "This module provides patterns and utilities for the Orionis Framework."
+)
+__copyright__ = "Copyright (c) 2024"
--- orionis/luminate/services/environment/__init__.py
+++ orionis/luminate/services/environment/__init__.py
@@ -3,3 +3,10 @@ from orionis.luminate.services.environment.env import Env
 __all__ = [
     "Env",
 ]
+
+__author__ = "Raúl Mauricio Uñate Castro"
+__description__ = (
+    "This module provides a singleton class for managing environment variables."
+    " It allows setting, getting, and unsetting environment variables, as well as"
+)
+__copyright__ = "Copyright (c) 2024"
