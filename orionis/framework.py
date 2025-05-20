780e35e2cf918990aae4bcdd88ced3476423ee29
Raul Mauricio Uñate Castro <runate@ccl.com.co>
2025-05-19 21:47:50 -0500

    📦 Release version 0.269.0

--- orionis/framework.py
+++ orionis/framework.py
@@ -5,7 +5,7 @@
 NAME = "orionis"
 
 # Current version of the framework
-VERSION = "0.269.0"
+VERSION = "0.270.0"
 
 # Full name of the author or maintainer of the project
 AUTHOR = "Raul Mauricio Uñate Castro"
