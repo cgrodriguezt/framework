16562bebe8c278eb5bec2c5c75322d8735f46598
Raul Mauricio Uñate Castro <runate@ccl.com.co>
2025-07-02 13:23:46 -0500

    📦 Release version 0.322.0

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 from implementacion import AbstractFakeClass, FakeClass
 from implementacion2 import Car, ICar
 from orionis.container.container import Container
+from orionis.container.facades.facade import Facade
 
 
 container = Container()
-
 with container.createContext():
-    container.singleton(ICar, Car)
+    container.transient(ICar, Car)
     container.scoped(AbstractFakeClass, FakeClass)
     print(container.make(AbstractFakeClass) is container.make(AbstractFakeClass))
 
-with container.createContext():
-    # container.scoped(ICar, Car)
-    print(container.make(ICar) is container.make(ICar))
+    class FacadeCar(Facade):
+        """
+        Log Facade class. This is the friendly interface for interacting with the logging service.
+        It's like the concierge of your application's logging system—always ready to help!
+        """
+
+        @classmethod
+        def getFacadeAccessor(cls):
+            """
+            Returns the service accessor for the logging system. In this case, it's the `ILogguerService`.
+            This is where the magic of the Facade pattern comes alive—connecting the interface to the actual service.
+            """
+            return AbstractFakeClass
+
+
+    print(FacadeCar.resolve() is FacadeCar.resolve())
