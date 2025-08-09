bb279be3949171a238f533d168f8f4647dce9a8c
Raul Mauricio Uñate Castro <runate@ccl.com.co>
2025-08-08 22:51:43 -0500

    📦 Release version 0.439.0

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
 from orionis.app import Orionis
 
+"""
+Initializes and configures an Orionis application instance for testing.
+
+This script creates an instance of the Orionis application, configures it for a testing environment
+by setting the execution mode to sequential, disabling persistence, and turning off web reporting.
+Finally, it initializes the application instance.
+
+Attributes
+----------
+app : Orionis
+    Instance of the Orionis application, configured for testing.
+"""
+
+# Create and configure the Orionis application instance
 app = Orionis()
+
+# Configure the application for testing
 app.setConfigTesting(
     execution_mode="sequential",
     persistent=False,
     web_report=False
 )
+
+# Initialize the application
 app.create()
