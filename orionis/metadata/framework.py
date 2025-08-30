c4b56dc5e2620729aa4d98f78513f10df7e89ec2
Raul Mauricio Uñate Castro <runate@ccl.com.co>
2025-08-29 22:07:34 -0500

    📦 Release version 0.563.0

--- orionis/metadata/framework.py
+++ orionis/metadata/framework.py
@@ -5,7 +5,7 @@
 NAME = "orionis"
 
 # Current version of the framework
-VERSION = "0.563.0"
+VERSION = "0.564.0"
 
 # Full name of the author or maintainer of the project
 AUTHOR = "Raul Mauricio Uñate Castro"
