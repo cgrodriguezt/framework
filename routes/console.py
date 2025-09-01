36b8cd39b92dac43c97018f093bf1dc03e99034d
Raul Mauricio Uñate Castro <runate@ccl.com.co>
2025-09-01 09:53:43 -0500

    📦 Release version 0.587.0

--- routes/console.py
+++ routes/console.py
@@ -2,7 +2,7 @@ from app.services.welcome_service import WelcomeService
 from orionis.console.args.argument import CLIArgument
 from orionis.support.facades.reactor import Reactor
 
-# Example route for console commands
+# Example console command definition
 Reactor.command('app:test', [WelcomeService, 'helloWorld']).timestamp().description('Command Test Defined as Route').arguments([
     CLIArgument(flags=["--name", "-n"], type=str, required=False)
 ])
