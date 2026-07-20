e8c23d856b0e0fa012a02f57aee5536e7daee463
rmunate <raulmauriciounate@gmail.com>
2026-07-20 17:34:13 -0500

    🔗 Fix __init__

---
+++ orionis/metadata/__init__.py
@@ -0,0 +1,25 @@
+from orionis.metadata.framework import (
+    API,
+    AUTHOR,
+    AUTHOR_EMAIL,
+    DESCRIPTION,
+    DOCS,
+    FRAMEWORK,
+    NAME,
+    PYTHON_REQUIRES,
+    SKELETON,
+    VERSION,
+)
+
+__all__ = [
+    "API",
+    "AUTHOR",
+    "AUTHOR_EMAIL",
+    "DESCRIPTION",
+    "DOCS",
+    "FRAMEWORK",
+    "NAME",
+    "PYTHON_REQUIRES",
+    "SKELETON",
+    "VERSION",
+]
