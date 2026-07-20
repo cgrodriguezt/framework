f48755f1929ce34bb17b8a54a0dc578b6ef774fb
rmunate <raulmauriciounate@gmail.com>
2026-07-20 17:33:10 -0500

    🛠️ Refactor interfaces

---
+++ orionis/http/enums/interfaces.py
@@ -0,0 +1,15 @@
+from enum import StrEnum
+
+class Interface(StrEnum):
+    """Represent the supported HTTP interface types.
+
+    Attributes
+    ----------
+    ASGI : str
+        Represents the ASGI interface.
+    RSGI : str
+        Represents the RSGI interface.
+    """
+
+    ASGI = "asgi"
+    RSGI = "rsgi"
