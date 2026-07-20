56cf5d93a237c66f0eae7c8846efc5bbc2ff53ad
rmunate <raulmauriciounate@gmail.com>
2026-07-20 17:45:33 -0500

    🌟 | REMOVE [FIELDS] - 2026-07-20

---
+++ orionis/schemas/fields.py
@@ -0,0 +1,7 @@
+from typing import Annotated as Field   # noqa: F401
+from typing import Literal as Choice    # noqa: F401
+from typing import Optional as Nullable # noqa: F401
+from typing import Union as AnyOf       # noqa: F401
+from typing import Final as Constant    # noqa: F401
+from typing import TypeAlias as Alias   # noqa: F401
+from typing import ClassVar as Static   # noqa: F401
