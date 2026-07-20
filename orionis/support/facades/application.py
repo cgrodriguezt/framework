cd672f1ec24fb88f1e759e89a28d1ba12d84ea36
rmunate <raulmauriciounate@gmail.com>
2026-07-20 16:54:02 -0500

    Changes - 2026-07-20 - application.py

---
+++ orionis/support/facades/application.py
@@ -0,0 +1,15 @@
+from orionis.container.facades.facade import Facade
+
+class Application(Facade):
+
+    @classmethod
+    def getFacadeAccessor(cls) -> str:
+        """
+        Return the facade accessor string for the unit test contract.
+
+        Returns
+        -------
+        str
+            String identifier for the service in the application container.
+        """
+        return "x-orionis-IApplication"
