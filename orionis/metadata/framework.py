40b68e511a1d26891ffa363aeb456082dda24d58
rmunate <raulmauriciounate@gmail.com>
2026-07-20 17:45:28 -0500

    🔗 | REMOVE [FRAMEWORK] - 2026-07-20

---
+++ orionis/metadata/framework.py
@@ -0,0 +1,32 @@
+# Full name of the author or maintainer of the project
+AUTHOR = "Raul Mauricio Uñate Castro"
+
+# Email address of the author or maintainer for contact purposes
+AUTHOR_EMAIL = "raulmauriciounate@gmail.com"
+
+# API URL to the project's JSON data
+API = "https://pypi.org/pypi/orionis/json"
+
+# Short description of the project or framework
+DESCRIPTION = (
+    "Orionis Framework — "
+    "Async-first full-stack framework for modern Python applications."
+)
+
+# URL to the project's documentation
+DOCS = "https://orionis-framework.com/"
+
+# URL to the project's main framework repository
+FRAMEWORK = "https://github.com/orionis-framework/framework"
+
+# Metadata for the Orionis Framework project
+NAME = "orionis"
+
+# Minimum Python version required to run the project
+PYTHON_REQUIRES = (3, 14)
+
+# URL to the project's skeleton or template repository (for initial setup)
+SKELETON = "https://github.com/orionis-framework/skeleton"
+
+# Current version of the framework
+VERSION = "0.756.0"
