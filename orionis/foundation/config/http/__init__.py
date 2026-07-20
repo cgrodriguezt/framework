1e1a9b83214d560d1c6dcb9ae71e1787c8b87355
rmunate <raulmauriciounate@gmail.com>
2026-07-20 17:51:10 -0500

    💎 [__init__] - Update

---
+++ orionis/foundation/config/http/__init__.py
@@ -0,0 +1,13 @@
+from orionis.foundation.config.http.entitites.cors import Cors
+from orionis.foundation.config.http.entitites.http import HTTP
+from orionis.foundation.config.http.entitites.proxies import HTTPProxies
+from orionis.foundation.config.http.entitites.rate_limit import HTTPRateLimit
+from orionis.foundation.config.http.entitites.security import HTTPSecurity
+
+__all__ = [
+    "HTTP",
+    "Cors",
+    "HTTPProxies",
+    "HTTPRateLimit",
+    "HTTPSecurity",
+]
