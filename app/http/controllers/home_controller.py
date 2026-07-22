b691cccbc813a249bd0de9db0347c3cb7693d298
rmunate <raulmauriciounate@gmail.com>
2026-07-22 15:33:11 -0500

    ⚡ [home_controller] - Add

---
+++ app/http/controllers/home_controller.py
@@ -0,0 +1,16 @@
+from orionis.http import HTMLResponse
+from orionis.http.base import BaseController
+from orionis.support.facades.view import View
+
+class HomeController(BaseController):
+
+    async def index(self) -> HTMLResponse:
+        """
+        Return the welcome page response.
+
+        Returns
+        -------
+        HTMLResponse
+            Rendered response for the welcome page.
+        """
+        return await View.make("welcome")
