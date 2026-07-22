fac77bd23167c1a58c2156a30c14f8d4229caa8d
rmunate <raulmauriciounate@gmail.com>
2026-07-22 07:46:51 -0500

    🎨 [__init__] - Update

--- orionis/cache/__init__.py
+++ orionis/cache/__init__.py
@@ -1,3 +1,9 @@
+from orionis.cache.cache_manager import CacheManager
 from orionis.cache.file_based_cache import FileBasedCache
+from orionis.cache.repository import CacheRepository
 
-__all__ = ["FileBasedCache"]
+__all__ = [
+    "CacheManager",
+    "CacheRepository",
+    "FileBasedCache",
+]
