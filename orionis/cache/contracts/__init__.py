18508bc0c16e916337b8cf52ae27bfba034c3a6f
rmunate <raulmauriciounate@gmail.com>
2026-07-22 07:46:51 -0500

    🧹 [__init__] - Update

--- orionis/cache/contracts/__init__.py
+++ orionis/cache/contracts/__init__.py
@@ -1,3 +1,9 @@
 from orionis.cache.contracts.file_based_cache import IFileBasedCache
+from orionis.cache.contracts.cache_manager import ICacheManager
+from orionis.cache.contracts.repository import ICacheRepository
 
-__all__ = ["IFileBasedCache"]
+__all__ = [
+    "ICacheManager",
+    "ICacheRepository",
+    "IFileBasedCache",
+]
