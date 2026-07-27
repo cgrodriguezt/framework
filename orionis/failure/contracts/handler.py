61bf1cc43edf84ba9dc1537cc7e0ae0cf4acf6aa
rmunate <raulmauriciounate@gmail.com>
2026-07-27 10:15:11 -0500

    🌀 [handler] - Update

--- orionis/failure/contracts/handler.py
+++ orionis/failure/contracts/handler.py
@@ -5,7 +5,7 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from orionis.console.output.contracts.console import IConsole
     from orionis.failure.entities.throwable import Throwable
-    from orionis.log.contracts.logger import ILogger
+    from orionis.logging.contracts.logger import ILogger
     from orionis.http.adapters.request.contracts.transport import TransportAdapter
     from orionis.http.request import Request
     from orionis.http.response import Response
