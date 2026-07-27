4aaf63663d5deaf885baf5771b3f450801ca8f8f
rmunate <raulmauriciounate@gmail.com>
2026-07-27 10:15:14 -0500

    🏗️ [logger] - Update

--- orionis/support/facades/logger.pyi
+++ orionis/support/facades/logger.pyi
@@ -1,5 +1,5 @@
 from orionis.container.contracts.facade import IFacade
-from orionis.log.contracts.logger import ILogger
+from orionis.logging.contracts.logger import ILogger
 
 class Log(ILogger, IFacade):
         ...
