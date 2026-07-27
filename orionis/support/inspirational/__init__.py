c1e40c6dd006aed518bcd23044638d6d97575a1f
rmunate <raulmauriciounate@gmail.com>
2026-07-27 13:27:43 -0500

    ⚡ [inspirational] - Add

---
+++ orionis/support/inspirational/__init__.py
@@ -0,0 +1,3 @@
+from orionis.support.inspirational.inspire import Inspire
+
+__all__ = ["Inspire"]
---
+++ orionis/support/inspirational/contracts/__init__.py
@@ -0,0 +1,3 @@
+from orionis.support.inspirational.contracts.inspire import IInspire
+
+__all__ = ["IInspire"]
