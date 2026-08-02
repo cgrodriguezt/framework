48026542ac1551c0f8de66ca00c068fb793e9ff8
rmunate <raulmauriciounate@gmail.com>
2026-08-01 22:01:54 -0500

    🔧 [m0000000010_create_role_has_permissions_table] - Update

--- database/migrations/m0000000010_create_role_has_permissions_table.py
+++ database/migrations/m0000000010_create_role_has_permissions_table.py
@@ -1,5 +1,4 @@
 from orionis.database import Migration
-from orionis.database.schema import Column, Comment, PrimaryKey
 from orionis.support.facades import Schema
 
 class CreateRoleHasPermissionsTable(Migration):
@@ -14,12 +13,11 @@ class CreateRoleHasPermissionsTable(Migration):
         None
             The table is created as a side effect.
         """
-        await Schema.create("role_has_permissions",
-            Column.bigInteger("permission_id").foreign("permissions.id").comment("Permission ID"),
-            Column.bigInteger("role_id").foreign("roles.id").comment("Role ID"),
-            PrimaryKey("permission_id", "role_id"),
-            Comment("Table to relate roles with permissions."),
-        )
+        async with Schema.create("role_has_permissions") as table:
+            table.bigInteger("permission_id").foreign("permissions.id").comment("Permission ID")
+            table.bigInteger("role_id").foreign("roles.id").comment("Role ID")
+            table.primaryKey("permission_id", "role_id")
+            table.comment("Table to relate roles with permissions.")
 
     async def down(self) -> None:
         """Drop the ``role_has_permissions`` table.
