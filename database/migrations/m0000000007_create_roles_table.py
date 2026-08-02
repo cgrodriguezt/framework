16823f4320b6a8cd8f35c227346e58aac57b1dd1
rmunate <raulmauriciounate@gmail.com>
2026-08-01 22:32:54 -0500

    🛡️ [m0000000007_create_roles_table] - Update

--- database/migrations/m0000000007_create_roles_table.py
+++ database/migrations/m0000000007_create_roles_table.py
@@ -16,8 +16,8 @@ class CreateRolesTable(Migration):
             table.id().comment("Role ID")
             table.string("name", 255).comment("Role Name")
             table.string("guard_name", 255).comment("Guard Name")
-            table.dateTime("created_at").nullable().comment("Created At")
-            table.dateTime("updated_at").nullable().comment("Updated At")
+            table.timestamps()
+
             table.unique("name", "guard_name")
             table.comment("Table to store roles.")
 
