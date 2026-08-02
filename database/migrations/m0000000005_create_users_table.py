32f31fb55be372bb16b954ad841dc86137d69a01
rmunate <raulmauriciounate@gmail.com>
2026-08-01 22:01:53 -0500

    🛠️ [m0000000005_create_users_table] - Update

--- database/migrations/m0000000005_create_users_table.py
+++ database/migrations/m0000000005_create_users_table.py
@@ -1,5 +1,4 @@
 from orionis.database import Migration
-from orionis.database.schema import Column, Comment
 from orionis.support.facades import Schema
 
 class CreateUsersTable(Migration):
@@ -13,17 +12,15 @@ class CreateUsersTable(Migration):
         None
             The table is created as a side effect.
         """
-        await Schema.create("users",
-            Column.id().comment("User ID"),
-            Column.string("name", 255).comment("Full Name"),
-            Column.string("email", 255).unique().comment("Email Address"),
-            Column.dateTime("email_verified_at").nullable().comment("Email Verification Timestamp"),
-            Column.string("password", 255).comment("Hashed Password"),
-            Column.string("remember_token", 100).nullable().comment("Remember Me Token"),
-            Column.dateTime("created_at").nullable().comment("Created At"),
-            Column.dateTime("updated_at").nullable().comment("Updated At"),
-            Comment("Table to store application users."),
-        )
+        async with Schema.create("users") as table:
+            table.id().comment("User ID")
+            table.string("name", 255).comment("Full Name")
+            table.string("email", 255).unique().comment("Email Address")
+            table.dateTime("email_verified_at").nullable().comment("Email Verification Timestamp")
+            table.string("password", 255).comment("Hashed Password")
+            table.string("remember_token", 100).nullable().comment("Remember Me Token")
+            table.timestamps()
+            table.comment("Table to store application users.")
 
     async def down(self) -> None:
         """
