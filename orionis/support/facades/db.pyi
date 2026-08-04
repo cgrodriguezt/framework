18d11547ace7417ff9bfc5635a2546437f757cfe
rmunate <raulmauriciounate@gmail.com>
2026-08-04 17:37:30 -0500

    🏗️ [db] - Update

--- orionis/support/facades/db.pyi
+++ orionis/support/facades/db.pyi
@@ -1,5 +1,14 @@
 from orionis.container.contracts.facade import IFacade
 from orionis.database.contracts.manager import IConnectionManager
+from orionis.orm.query.raw_builder import RawQueryBuilder
 
 class DB(IConnectionManager, IFacade):
-    ...
+    @classmethod
+    def table(
+        cls,
+        name: str,
+        *,
+        alias: str | None = None,
+        connection: str | None = None,
+    ) -> RawQueryBuilder: ...
+
