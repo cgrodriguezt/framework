5e2016a9a3dd112b6812de0d2e591aa7e49e1a54
rmunate <raulmauriciounate@gmail.com>
2026-08-06 23:00:13 -0500

    ⚡ [status_command] - Update

--- orionis/console/commands/migrate/status_command.py
+++ orionis/console/commands/migrate/status_command.py
@@ -1,7 +1,6 @@
 from orionis.console.commands.migrate.base_command import MigrationCommand
 from orionis.database.migrations.migrator import Migrator
 
-
 class MigrateStatusCommand(MigrationCommand):
     """Report which migrations are applied and which are pending."""
 
