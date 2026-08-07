1eb3d7eee6b66a2d1d973defd5782c2c4044427a
rmunate <raulmauriciounate@gmail.com>
2026-08-06 22:50:12 -0500

    💎 [commands] - Update

--- orionis/console/core/commands.py
+++ orionis/console/core/commands.py
@@ -1,7 +1,11 @@
 from orionis.console.commands.make.make_provider import MakeProvider
 from orionis.console.commands.make.task_listener_command import MakeTaskListener
+from orionis.console.commands.migrate.fresh_command import MigrateFreshCommand
 from orionis.console.commands.migrate.migrate_command import MigrateCommand
+from orionis.console.commands.migrate.refresh_command import MigrateRefreshCommand
+from orionis.console.commands.migrate.reset_command import MigrateResetCommand
 from orionis.console.commands.migrate.rollback_command import MigrateRollbackCommand
+from orionis.console.commands.migrate.status_command import MigrateStatusCommand
 from orionis.console.commands.support.optimize import OptimizeCommand
 from orionis.console.commands.support.optimize_clear import OptimizeClearCommand
 from orionis.console.commands.support.list import HelpCommand
@@ -30,7 +34,11 @@ def get_core_commands_mapping() -> tuple:
         MakeProvider,
         MakeTaskListener,
         MigrateCommand,
+        MigrateFreshCommand,
+        MigrateRefreshCommand,
+        MigrateResetCommand,
         MigrateRollbackCommand,
+        MigrateStatusCommand,
         ScheduleListCommand,
         ScheduleWorkCommand,
         TestCommand,
